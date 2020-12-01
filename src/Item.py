1ca7a22a8f0f8aab4c26673c2d4d2f24f1bfe92d
Ricardo Delgado <ridel007@gmail.com>
2020-12-01 17:41:53 -0500

    Implementig item class

--- src/Item.py
+++ src/Item.py
@@ -1,10 +1,43 @@
+from random import randint
+
+
 class Item:
 
     def __new__(cls, *args, **kwargs):
-        pass
+        return super(Item, cls).__new__(cls)
 
     def __init__(self):
-        pass
+        self._item_id = 0
+        self._name = 'none'
+        self._is_consumible = False
+        self._is_equipable = False
+        self._action_when_consume = 0
+        self._feature = 'Este objeto está pegado al piso. Que vergüenza'
+
+        self.define_item()
+
+    def define_item(self, max_item_value=100, max_item_with_object=21):
+        self._item_id = randint(1, max_item_value)
+
+        if self._item_id < max_item_with_object:
+            # Require search for item_id in data base
+            self.looking_for_item()
+            return True
+        else:
+            return False
+
+    def looking_for_item(self):
+        # Search in data base for item
+        # Sample emulating data base response
+        self._item_id = 1
+        self._name = 'Cerveza'
+        self._is_consumible = True
+        self._is_equipable = False
+        self._action_when_consume = 20
+        self._feature = 'Me siento un poco mareado, pero ya no tengo miedo'
+
+        # return self._item_id, self._name, self._is_consumible, self._is_equipable, self._action_when_consume,
+        # self.feature
 
     def __del__(self):
-        pass
+        print('Kill Item')
--- src/Stage.py
+++ src/Stage.py
@@ -14,27 +14,22 @@ class Stage:
         rows = 20
         cols = 20
         # New game or load and saved game
-        # if new_game:
-        print('Aqui')
-        for i in range(rows):
-            for j in range(cols):
-                print("agregando")
-                cells.append(self.create_cell(i, j, 0))
+        if new_game:
+            for i in range(rows):
+                for j in range(cols):
+                    cells.append(self.create_cell(i, j, 0))
 
         print(len(cells))
-
-        for i in cells:
-            for j in cells:
-                print(j.x_coord)
-                print('Recorriendo')
+        print(cells)
 
     def __del__(self):
         print("Destroy stage")
 
     @staticmethod
     def create_cell(x_coord, y_coord, z_coord):
-        return Cell(x_coord, y_coord, z_coord)
+        # return Cell(x_coord, y_coord, z_coord)
+        return 1
 
 
 # For testing
-s = Stage(True);
+s = Stage(True)
