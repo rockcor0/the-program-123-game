813340bbb95d766b6761076117ade3ba3add900a
Ricardo Delgado <ridel007@gmail.com>
2020-12-01 07:37:10 -0500

    Creating cell

--- src/Cell.py
+++ src/Cell.py
@@ -1,7 +1,17 @@
 class Cell:
 
-    def __init__(self):
-        self.x_coord = 0
-        self.y_coord = 0
-        self.z_coord = 0
+    def __new__(cls, *args, **kwargs):
+        print('Creating cell - new')
+        return super(Cell, cls).__new__(cls)
+
+    def __init__(self, x_coord, y_coord, z_coord):
+        print('Initialize cell - init')
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.z_coord = z_coord
+
+#Sample for testing
+cell = Cell(1,1,10)
+print(cell.z_coord)
+
 
