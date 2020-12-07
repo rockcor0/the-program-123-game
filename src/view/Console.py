3f0605c47ac68e04ab8d4eee0e029a97f2a0693d
Ricardo Delgado <ridel007@gmail.com>
2020-12-07 18:45:30 -0500

    Commands

--- src/view/Console.py
+++ src/view/Console.py
@@ -1,2 +1,29 @@
 class Console:
-    pass
+
+    def __new__(cls, *args, **kwargs):
+        return super(Console, cls).__new__(cls)
+
+    def __init__(self):
+        print('Hello, init the console')
+
+    def write_command(self):
+        command = input('Please write something: ')
+
+        if self.exit_from_console(command):
+            print('Bye')
+
+        else:
+            print(command)
+            self.write_command()
+
+    @staticmethod
+    def exit_from_console(command):
+        if command == 'exit':
+            return True
+        else:
+            return False
+
+
+# For testing
+c = Console()
+c.write_command()
