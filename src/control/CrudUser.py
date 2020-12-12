3b19d7213f3dc7bd2b71d75cc26dc8fba61e71bb
Ricardo Delgado <ridel007@gmail.com>
2020-12-11 21:45:16 -0500

    Crud user

--- src/control/CrudUser.py
+++ src/control/CrudUser.py
@@ -16,9 +16,13 @@ def create_player(name, email, age):
     return firebase.post(user_uri, data)
 
 def read_player(player_id):
+    # Update for read only a user
     result = firebase.get(user_uri, '')
     print(result)
     return result
+
+def update_player(player_id):
+    pass
 
 create_player('Ricardo Delgado', 'ridel007@gmail.com', 37)
 read_player(' ')
