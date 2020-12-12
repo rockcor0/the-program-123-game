ec83f60b014b71046758e8c5d18fbe893c790608
Ricardo Delgado <ridel007@gmail.com>
2020-12-11 20:36:37 -0500

    DBConection

--- src/control/DbConection.py
+++ src/control/DbConection.py
@@ -1,10 +1,10 @@
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://the-program-123-game.firebaseio.com/', None)
-data = {
+data = {'123': {
   'username':'rockcor',
   'name':'Ricardo Delgado',
   'email':'ridel007@gmail.com'
-}
+}}
 result = firebase.post('/users-test', data)
 print(result)
