96aab4c903982b3c4c998ee87fb92b65261d85fe
Ricardo Delgado <ridel007@gmail.com>
2020-12-12 19:04:11 -0500

    Update player

--- src/control/CrudUser.py
+++ src/control/CrudUser.py
@@ -3,9 +3,9 @@ from firebase import firebase
 from src.control import DbConection
 
 firebase = DbConection.firebase
-user_uri = '/users-test'
+user_uri = DbConection.uri_player
 
-
+# Create the player in the database
 def create_player(name, email, age):
     data = {
         'name': name,
@@ -15,29 +15,33 @@ def create_player(name, email, age):
 
     return firebase.post(user_uri, data)
 
-
+# Read de player from database
 def read_player(player_id):
     # Update for read only a user
     result = firebase.get(user_uri, '')
     print(result)
     return result
 
-
+# Update de player
 def update_player(player_id, name, email, age):
     data = {
         'name': name,
         'email': email,
         'age': age
     }
-    result = firebase.put(user_uri+'/'+player_id, 'name', name)
+
+    result = firebase.put(user_uri, player_id, data)
     print(result)
     return result
 
-
+# Delete the player
 def delete_player(player_id):
-    pass
+    result = firebase.delete(user_uri, player_id)
+    print(result)
+    return result
 
 
 # create_player('Ricardo Delgado', 'ridel007@gmail.com', 37)
 read_player(' ')
 update_player('-MOJ_fJScoQsBBHFdYnW', 'Lilly', 'lipadugi', 32)
+delete_player('-MOJeAneLr9EZsu-9awB')
--- src/control/DbConection.py
+++ src/control/DbConection.py
@@ -1,6 +1,8 @@
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://the-program-123-game.firebaseio.com/', None)
+uri_player = '/users-test'
+
 data = {'123': {
   'username':'rockcor',
   'name':'Ricardo Delgado',
