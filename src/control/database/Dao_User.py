96b114100196964f9032457fd7fa6a17dadad2ae
Ricardo Delgado <ridel007@gmail.com>
2021-01-20 21:47:41 -0500

    Dao

--- src/control/database/Dao_User.py
+++ src/control/database/Dao_User.py
@@ -6,7 +6,7 @@ firebase = DbConection.firebase
 user_uri = DbConection.uri_player
 
 #Está un poco quedaado el proyecto
-#Continuar con el proceso
+#Continuar con el proceso 
 # Create the player in the database
 def create_player(name, email, age):
     data = {
